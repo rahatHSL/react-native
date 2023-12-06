495703fa972eaaa1b7dc94d39bc52ad64dc10e0e
rahat <mirrahat00@gmail.com>
2023-12-07 00:45:24 +0600

    introduce login page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { View, SafeAreaView, Text, StyleSheet, ScrollView } from 'react-native'
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Button,
+  useColorScheme,
+} from 'react-native'
 import { Rounded } from './assets/styles/common'
 import { DataFetch } from './components/FetchData'
 
@@ -13,20 +22,39 @@ function App(): Element {
     const data = await DataFetch.getPost()
     setData(data)
   }
-
+  const color = useColorScheme()
   return (
     <SafeAreaView>
       <ScrollView>
         <ScrollView>
-          <View style={styles.component}>
-            {data.map((item: any, index) => (
+          <View style={styles.container}>
+            {/* {data.map((item: any, index) => (
               <Rounded text={item.name} key={index} color={'red'} />
-            ))}
+            ))} */}
             {/* <Rounded color={'blue'} />
             <Rounded color={'purple'} />
             <Rounded color={'red'} />
             <Rounded color={'blue'} />
             <Rounded color={'purple'} /> */}
+            <View
+              style={{
+                backgroundColor: 'gray',
+                justifyContent: 'center',
+                padding: 25,
+                borderRadius: 22,
+                opacity: 0.7,
+                shadowColor: 'gray',
+                shadowRadius: 300,
+                shadowOpacity: 0.6,
+                shadowOffset: { height: 50, width: 22 },
+              }}
+            >
+              <Text>Enter Phone</Text>
+              <TextInput keyboardType="phone-pad" placeholder="Enter Phone no."></TextInput>
+              <Text>Enter Password</Text>
+              <TextInput secureTextEntry={true} placeholder="Enter Password"></TextInput>
+              <Button title="Login" />
+            </View>
           </View>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             {/* <IncrementViews /> */}
@@ -38,11 +66,14 @@ function App(): Element {
 }
 
 const styles = StyleSheet.create({
-  component: {
-    height: 'auto',
-    backgroundColor: 'green',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+  container: {
+    flex: 1,
+    height: 740,
+    backgroundColor: '#F1e4dd',
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: 10,
   },
 })
 
